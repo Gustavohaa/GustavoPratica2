27555a1db2aeabaaba39407ec11f8a6ce5207b6f
Gustavohaa <111452073+Gustavohaa@users.noreply.github.com>
2023-11-10 14:32:36 -0300

    Update Application.java

--- Pratica2/src/main/java/pratica2/Application.java
+++ Pratica2/src/main/java/pratica2/Application.java
@@ -1,9 +1,6 @@
 package pratica2;
 
 import java.util.Scanner;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class Application {
 	public static void main(String[] args) {
